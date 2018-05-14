e72160ae927a647738ccb606c61bcd7a46af8293
Zaki Mukhammad <zakimuhammad20@gmail.com>
2018-05-15 03:04:00 +0700

    Tambah backend

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@ require_once "core/init.php";
 
 if(isset($_GET['id'])){
     if(hapus_data($_GET['id'])){
-        header('Location:index.php');
+        header('Location:index.php');    
     } else {
         echo 'gagal menghapus data';
     }
