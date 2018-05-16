d13db1b2fcdae65153ee22946e8279ebd273b800
DamarAd <damaradyun@gmail.com>
2018-05-16 22:12:54 +0700

    login page

--- login.php
+++ login.php
@@ -1,43 +1,60 @@
-<?php
-    require_once 'core/init.php';
-    
-    if(isset($_SESSION['user'])){
-        header('Location:index.php');
-    } else {
-    
-    $error = '';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Login</title>
+        <link rel="stylesheet" href="bt/css/bootstrap.min.css">
+    </head>
+    <body>
 
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    <?php
+        require_once 'core/init.php';
         
-        if (!empty(trim($username)) && !empty(trim($password))) {
+        if(isset($_SESSION['user'])){
+            header('Location:index.php');
+        } else {
+        
+        $error = '';
+
+        if (isset($_POST['submit'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
             
-            if (cek_login($username,$password)) {
-                $_SESSION['user'] = $username;
-                header('Location: index.php');
+            if (!empty(trim($username)) && !empty(trim($password))) {
+                
+                if (cek_login($username,$password)) {
+                    $_SESSION['user'] = $username;
+                    header('Location: index.php');
+                } else {
+                    $error = 'Username atau password salah';
+                }
+                
             } else {
-                $error = 'Username atau password salah';
+                $error = 'username dan password wajib diisi'; 
             }
             
-        } else {
-            $error = 'username dan password wajib diisi'; 
         }
         
-    }
-       
-?>
+    ?>
 
-<form action="" method="post">
-    <label for="username">username</label>
-    <input type="text" name="username" value="" required><br><br>
+    <form action="login.php" method="post">
+        <div class="form-group">    
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control" value="" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control" value="" required>
+        </div>
+        <div id="error" class="text-danger"><?=$error ?></div>
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        
+    </form>
 
-    <label for="password">password</label>
-    <input type="password" name="password" value="" required><br><br>
+    <?php } ?> 
 
-    <div id="error"><?=$error ?></div><br>
-
-    <input type="submit" value="submit" name="submit">
-</form>
-
-<?php } ?> 
+    <script href="bt/js/bootstrap.min.js"></script>
+    </body>
+</html>
